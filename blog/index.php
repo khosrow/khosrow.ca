1dd82a11af6ce05619dd63709f1ef45dfa76ccd2
Khosrow E <khosrow.ebrahimpour@gmail.com>
2012-09-03 15:03:15 -0400

    Initial commit

---
+++ blog/index.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+<meta HTTP-EQUIV="REFRESH" content="0; url=http://blog.khosrow.ca">
+</head>
+<body>
+</body>
---
+++ code/index.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+<meta HTTP-EQUIV="REFRESH" content="0; url=http://code.khosrow.ca">
+</head>
+<body>
+</body>
